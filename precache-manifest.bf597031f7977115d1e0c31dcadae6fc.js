92fb8ef32a95f6c17df37ad0be096f39b8c43ff2
ishawakankar <iwakankar@hotmail.com>
2019-01-28 15:52:00 +0530

    Updates

---
+++ precache-manifest.bf597031f7977115d1e0c31dcadae6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zomatoAPi-ghPages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7709298dc4ea4fae24c3",
+    "url": "/zomatoAPi-ghPages/static/js/runtime~main.7709298d.js"
+  },
+  {
+    "revision": "8d83235ad44d0d9cdea2",
+    "url": "/zomatoAPi-ghPages/static/js/main.8d83235a.chunk.js"
+  },
+  {
+    "revision": "9a7a8e6e78d03554aeca",
+    "url": "/zomatoAPi-ghPages/static/js/1.9a7a8e6e.chunk.js"
+  },
+  {
+    "revision": "8d83235ad44d0d9cdea2",
+    "url": "/zomatoAPi-ghPages/static/css/main.a13a9f03.chunk.css"
+  },
+  {
+    "revision": "24ee55f2e18b809b33c3a34efe17d286",
+    "url": "/zomatoAPi-ghPages/index.html"
+  }
+];
